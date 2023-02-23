2b7a7a0b72d0bf2db4118dab33f16b3b75c31c9c
MagBuddybo <leratothemba@gmail.com>
2023-02-23 03:45:08 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isdigit - Check if charater is a digit
- * @x: The number to be checked
- * Return: 1 for a character that will be a digit or 0 for anything else
+ *_isdigit - Check if charater is a digit
+ *@x: The number to be checked
+ *Return: 1 for a character that will be a digit or 0 for anything else
  */
 int _isdigit(int x)
 {
