031a019fd8345b463f28858bad843b797ebdee61
MagBuddybo <leratothemba@gmail.com>
2023-03-08 14:16:05 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _sqrt - calculates natural square root
+ * @n: number to calculate the square root
+ * @i: iterate number
+ * Return: the natural square root
+ */
+int _sqrt_recursion(int n)
+{
